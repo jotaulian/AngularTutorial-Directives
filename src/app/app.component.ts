05ec9bd4d441a81bd6b1dbe7d678bb2c219f3065
jotaulian <juluegglepalomo@gmail.com>
2022-01-05 18:31:09 +0100

    Angular directives exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Listado de Personas';
+  nombreInput: string = '';
+  apellidoInput: string = '';
+
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Calva'),
+    new Persona('Pablo', 'Perdido'),
+  ];
+
+  agregarPersona(){
+    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona1);
+  }
 }
---
+++ src/app/persona.model.ts
@@ -0,0 +1,15 @@
+export class Persona{
+/*  nombre: string = '';
+  apellido: string = '';
+
+  constructor(nombre: string, apellido: string){
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }*/
+
+  //Es lo mismo que lo de arriba
+  constructor(public nombre: string, public apellido: string){}
+
+
+
+}
